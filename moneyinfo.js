77f4c14406b539eb0316f575e916ec0154e445a2
charmypappittosan <mikimaku69@gmail.com>
2022-02-17 01:24:57 +0600

    third commit

--- moneyinfo.js
+++ moneyinfo.js
@@ -1,8 +1,8 @@
 // function for total income
-function getIncome() {
-  const randomIncome = Math.round(Math.random() * 10000);
-    return randomIncome;
-}
+// function getIncome() {
+//   const randomIncome = Math.round(Math.random() * 10000);
+//     return randomIncome;
+
 function value(event){
     const costValue = document.getElementById(event);
     const totalValue = parseInt(costValue.value);
@@ -11,12 +11,30 @@ function value(event){
 }
 
 document.getElementById('calculate').addEventListener('click',function(){
-    const income=document.getElementById('income');
-    income.value=getIncome();
+    // const income=document.getElementById('income');
+    // income.value=getIncome();
+    const income=value('income')
     const food=value('food');
     const rent=value('rent');
     const cloth=value('cloth');
-    const income = document.getElementById(");
+    const totalExpense = document.getElementById('total-expense');
+    const totalCalculatedExpense=food+rent+cloth;
+    totalExpense.innerText=totalCalculatedExpense;
+    
+    const balanceTotal=document.getElementById('balance');
+    const totalBalance=income-totalCalculatedExpense;
+    balanceTotal.innerText=totalBalance;
 
 
+});
+document.getElementById('save').addEventListener('click',function(){
+    const income2=value('income');
+    const saveInput=value('save-input');
+    const savingAmount = document.getElementById("saving-amount");
+    const saverTax=saveInput/100;
+    const totalSavingAmount=income2*saverTax;
+    savingAmount.innerText = parseFloat(totalSavingAmount);
+    const remainingBalance = document.getElementById("remaining-balance");
+    const remainingTotalBalance=income2-totalSavingAmount;
+    remainingBalance.innerText = parseFloat(remainingTotalBalance);
 });
