f78b87d4ec6b5b4d89e14bc85c79438b47d5c451
charmypappittosan <mikimaku69@gmail.com>
2022-02-17 10:38:16 +0600

    5th commit

--- moneyinfo.js
+++ moneyinfo.js
@@ -10,14 +10,15 @@ function value(event){
     const costValue = document.getElementById(event);
     const totalValue = parseInt(costValue.value);
     const validationMassege=document.getElementById('validation-massege');
-    if(totalValue>0){
-        costValue.value=totalValue;
-        validationMassege.style.display = "none";
-    return totalValue;
+    if(totalValue>0 ){
+              costValue.value = totalValue;
+              validationMassege.style.display = "none";
+              return totalValue;
+       
     }
     else{
-        validationMassege.style.display = "block";
-      const emptyfield = "";
+        const emptyfield = "";
+      validationMassege.style.display = "block";
       costValue.value=emptyfield;
       return emptyfield;
     }
@@ -35,19 +36,22 @@ document.getElementById('calculate').addEventListener('click',function(){
     const rent=value('rent');
     const cloth=value('cloth');
     const totalExpense = document.getElementById('total-expense');
-    const totalCalculatedExpense=food+rent+cloth;
-    totalExpense.innerText=totalCalculatedExpense;
+    const totalCalculatedExpense = parseInt(food + rent + cloth);
+    totalExpense.innerText = totalCalculatedExpense;
     
     // calculating total expenses
     const balanceTotal=document.getElementById('balance');
     const totalBalance=income-totalCalculatedExpense;
+   
     if(totalBalance>=0){
-        
         balanceTotal.innerText = totalBalance;
+       
     }
     else{
-        const validation=document.getElementById('balance-validation');
+         const validation = document.getElementById("balance-validation");
         validation.style.display='block';
+
+    
     }
 
 
@@ -65,6 +69,16 @@ document.getElementById('save').addEventListener('click',function(){
 
     // calculating remaining balance
     const remainingBalance = document.getElementById("remaining-balance");
-    const remainingTotalBalance=income2-totalSavingAmount;
-    remainingBalance.innerText = parseFloat(remainingTotalBalance);
+    const balanceTotal = document.getElementById("balance");
+    const balancesubTotal=parseInt(balanceTotal.innerText);
+    const remainingTotalBalance=balancesubTotal-totalSavingAmount;
+
+    if(remainingTotalBalance>=0){
+         remainingBalance.innerText = parseFloat(remainingTotalBalance);
+    }
+    else{
+        const emptybalance='00';
+        remainingBalance.innerText=emptybalance;
+
+    }
 });
